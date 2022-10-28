b8e38558fface590b1881944e9446ccab4dc9800
NicolasRSJ <nicolassjesus@gmail.com>
2022-10-28 17:58:40 -0300

    TableSort Complete

---
+++ src/Redux/Actions/index.js
@@ -0,0 +1,13 @@
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+
+const configAddTime = {
+    name: 'addtime',
+    initialState: {
+        time: 0
+    },
+    reducer: {
+        addTime: (state) => {
+            
+        }
+    }
+}
